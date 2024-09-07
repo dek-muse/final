ff304989662b332f9a356053e0f3f32db96e5830
dek-muse <mohdeeqibnuipraahim@gmail.com>
2024-09-07 13:12:02 +0300

    done

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,52 @@
-  const express = require('express');
-  const mongoose = require('mongoose');
-  const cors = require('cors');
-  const errorHandler = require('./utils/errorHandler');
-  const path = require('path');
- 
-  require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const errorHandler = require('./utils/errorHandler');
+const path = require('path');
 
-  const app = express();
+require('dotenv').config();
 
-  // Middleware
-  app.use(express.json());
-  app.use(cors()); // Enable CORS
+const app = express();
 
-  // Connect to Database
-  mongoose.connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000, // Adjust as needed
-    socketTimeoutMS: 45000, // Adjust as needed
+// Middleware
+app.use(express.json());
+app.use(cors()); // Enable CORS
+
+// Connect to Database
+mongoose.connect(process.env.MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000, // Adjust as needed
+  socketTimeoutMS: 45000, // Adjust as needed
+})
+  .then(() => {
+    console.log('Database connection successful 🤞');
   })
-    .then(() => {
-      console.log('Database connection successful 🤞');
-    })
-    .catch((err) => {
-      console.error('MongoDB connection error:', err);
-      process.exit(1); // Exit the process with failure
-    });
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit the process with failure
+  });
 
-  // Import Routes
-  // const authRoutes = require('../routes/authRoutes');
-  const teacherRoutes = require('./routes/teacherRoutes');
-  const userRouter = require('./routes/userRouter');
+// Import Routes
+// const authRoutes = require('../routes/authRoutes');
+const teacherRoutes = require('./routes/teacherRoutes');
+const userRouter = require('./routes/userRouter');
 
-  // Use Routes
-  // app.use('/api/auth', authRoutes);
-  app.use('/api/auth', userRouter);
-  app.use('/api/teachers', teacherRoutes);
+// Use Routes
+// app.use('/api/auth', authRoutes);
+app.use('/api/auth', userRouter);
+app.use('/api/teachers', teacherRoutes);
 
-  // Error Handling Middleware
-  app.use(errorHandler);
-  app.use('/uploads', express.static('uploads')); // Serve static files from the uploads directory
+// Serve static files from the uploads directory
+app.use('/uploads', express.static('uploads'));
 
+// Add Hello World Route
+app.get('/', (req, res) => {
+  res.send('Hello World 🌍');
+});
 
- 
-
+// Error Handling Middleware
+app.use(errorHandler);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -54,6 +57,7 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
 // Define the port
 const PORT = process.env.PORT || 5000;
 
