bfbde8224e7b009d78451a3fd41b26e3fe7a4f26
deek-muse <deekibraa@gmail.com>
2024-09-28 22:21:37 +0300

    sdone

--- backend/controllers/teacherController.js
+++ backend/controllers/teacherController.js
@@ -1,17 +1,9 @@
 const Teacher = require('../models/Teacher');
-const multer = require('multer');
 const path = require('path');
 
 // Configure multer for file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
-const upload = multer({ storage: storage });
+ 
+ 
 
 // Create a new teacher
 exports.createTeacher = async (req, res) => {
