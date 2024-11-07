7aa33653d93e07275d208f348eedadfb4c3653ac
dek-muse <deekibraa@gmail.com>
2024-11-07 23:20:52 +0300

    dpone

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -7,8 +7,7 @@ import homeimage from '../assets/homeimage.svg';
 import { NavLink } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import work1 from '../../assets/work1.svg'; 
-import work2 from '../assets/work2.svg'
+ import work2 from '../assets/work2.svg'
 import work3 from '../assets/work3.svg'
 
 // Animated count component
@@ -246,7 +245,7 @@ const Home = () => {
   <div className="flex flex-col md:flex-row items-center gap-10">
     <div className="w-full md:w-1/2">
     
-      <img src={work1} alt="Education Illustration" className="w-full "/>
+      <img src={work2} alt="Education Illustration" className="w-full "/>
 
     </div>
     <div className="w-full md:w-1/2 mt-8">
